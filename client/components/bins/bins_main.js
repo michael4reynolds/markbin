7c5b703fdffa3b9e064e36e7c95f533ee328ec88
Michael Reynolds <michael4reynolds@gmail.com>
2017-09-11 14:29:11 -0400

    Begin BinsEditor

--- client/components/bins/bins_main.js
+++ client/components/bins/bins_main.js
@@ -1,12 +1,15 @@
 import React, {Component} from 'react'
 import {createContainer} from 'meteor/react-meteor-data'
 import {Bins} from "../../../imports/collections/bins"
+import BinsEditor from "./bins_editor"
 
 class BinsMain extends Component {
   render() {
     return (
       <div>
-        <div>BinMain</div>
+        <div>
+          <BinsEditor bin={this.props.bin}/>
+        </div>
       </div>
     )
   }
