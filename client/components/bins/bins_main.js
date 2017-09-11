1861229565266873ff181bff3843ccfcefadf2b1
Michael Reynolds <michael4reynolds@gmail.com>
2017-09-11 13:56:41 -0400

    Just the right bin

--- client/components/bins/bins_main.js
+++ client/components/bins/bins_main.js
@@ -1,14 +1,19 @@
-import React, {Component} from 'react';
+import React, {Component} from 'react'
+import {createContainer} from 'meteor/react-meteor-data'
+import {Bins} from "../../../imports/collections/bins"
 
 class BinsMain extends Component {
   render() {
-    console.log(this.props.match.params.binId)
     return (
       <div>
         <div>BinMain</div>
       </div>
-    );
+    )
   }
 }
 
-export default BinsMain;
+export default createContainer(props => {
+  const {binId} = props.match.params
+  Meteor.subscribe('bins')
+  return {bin: Bins.findOne(binId)}
+}, BinsMain)
