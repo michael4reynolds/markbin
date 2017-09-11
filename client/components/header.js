04fa7d147e274504cd3f9d3f042cbb5615ac58d4
Michael Reynolds <michael4reynolds@gmail.com>
2017-09-11 12:59:44 -0400

    Programmatic Navigation

--- client/components/header.js
+++ client/components/header.js
@@ -1,15 +1,28 @@
 import React, {Component} from 'react';
-import {Link} from 'react-router-dom'
+import {Link, Redirect} from 'react-router-dom'
 import Accounts from './accounts'
 
 class Header extends Component {
+  state = {
+    binId: false,
+  }
+
   onBinClick = e => {
     e.preventDefault()
 
-    Meteor.call('bins.insert')
+    Meteor.call('bins.insert', (error, binId) => this.setState({binId}))
   }
 
   render() {
+    const {binId} = this.state
+
+    if (binId) {
+      this.state.binId = false
+      return (
+        <Redirect push to={`/bins/${binId}`}/>
+      )
+    }
+
     return (
       <nav className="nav navbar-default">
         <div className="navbar-header">
